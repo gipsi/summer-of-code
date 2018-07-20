dc35e353d9966d1950b5eb07f42bf447bcc0401a
gipsi <chantstream@live.co.uk>
2018-07-21 00:54:15 +0100

    Create wk1-d1.py
    Notes and exercises

---
+++ wk1-d1.py
@@ -0,0 +1,137 @@
+# Summer of code Notes
+# Week 1 day 1
+# First simple arithmetic exercises
+
+# print(3)
+
+# print(1+2)
+
+# print(1+2)
+
+# print(2*3)
+# first error message! Indentationerror: unexpected indent
+
+# print(5-8)
+# second error message. Indentationerror: unexpected indent again :|
+
+# print(9/2)
+
+# print(5 * (12-8) + -15)
+# print(98 + (59872 / (13*8)) * -51)
+
+# modulus: gives you the remainder after division
+# print(10 % 2)
+
+# power and bitshift
+# credit: alteredco "mathletes"
+# print(2**8)
+# print(1<<9)
+
+# Basic homework exercises for certification:
+# I'm on the DIY track but I'll do as much as I can anyway
+
+# Hours in a year. How many hours are in a year?
+# print(24*365)
+
+# Minutes in a decade. How many minutes are in a decade?
+# minutes in an day:
+# print(60*24)
+# = 1440
+# minutes in a year:
+# print(1440*365) 
+# first try just got the prompt back - I forgot to uncomment!
+# The answer: 525600 minutes in a year
+# minutes in a decade:
+# print(525600*10)
+# = 5256000
+# seconds in a day
+# print(1440*60)
+# = 86400
+
+
+# Extra questions:
+# Seconds in a decade:
+# print(5256000*60)
+# 315360000
+# How many minutes in a leap year: 
+# print(525600+1440)
+# = 527040
+# How many seconds in a leap year:
+# print(527040*60)
+# 31622400
+
+# My age in seconds:
+# print(315360000*6)
+# = 1892160000
+# print(31*5)
+# = 155
+# print(30*2)
+# = 60
+# print(155+60)
+# = 215
+# print (215*1440)
+# = 309600
+# print(309600*60)
+# = 18576000
+# print(18576000-86400)
+# = 18489600
+# print(18489600-600)
+# = 18489000
+# print(1892160000-18489000)
+# = 1873671000 My age in seconds!
+
+
+
+
+# This was the first go where I did it a different way.
+# Your age in seconds. How many seconds old are you? (I'm not going to check your answer, so be as accurate—or not—as you want.)
+# print(525600*58)
+# = 30484800
+# FullyQualifiedErrorId: CommandNotFound Exception
+# I accidently put a dot between python and calc.py
+# print(1440*16)
+# = 23040
+# print(30484800+23040)
+# = 30507840
+# print(1440*31)
+# = 44640
+# print(1440*30)
+# oops! I typed in just python and got >>>
+# I tried  >>> calc.py and got Traceback (most recent 
+# call last): File"<stdin", line 1. in <module>
+# NameError: name 'calc' is not defined
+# >>> python calc.py
+# File "<stdin>", line 1
+# python calc.py
+#           caret
+# SyntaxError: invalid syntax
+# >>> quit()
+# Finally! 
+# = 43200 
+# print(43200+44540)
+# IMPORTANT NOTE: remember to save file
+# = 87740
+# print(30507840+87740)
+# = 305995580
+#
+
+
+
+
+
+# Andreea Visanoiu: I'm 48618000 seconds old hahaha.  Calculate @Andreea Visanoiu's age.
+
+# Here are some tougher questions:
+
+# How many days does it take for a 32-bit system to
+# timeout, if it has a bug with integer overflow?
+
+# ref: https://en.wikipedia.org/wiki/Year_2038_problem
+
+# How about a 64-bit system?
+# Calculate your age accurately based on your birthday 
+#(maybe use time of day e.g. 8:23am if you know it, use # 12:00 noon midday) - you will need Python modules.
+
+# ref: https://www.learnpython.org/en/Modules_and_Packages
+
+# print(24*365)
