6ea47a80e7234270a16d4e7c7dd620c3a96ca2af
gipsi <chantstream@live.co.uk>
2018-07-29 11:56:43 +0100

    Create cool_int.py

---
+++ cool_int.py
@@ -0,0 +1,19 @@
+# week 2 Day 4
+
+# Modules - cool_int
+
+# access to function and variable
+
+def to_eng(n):
+	if n == 5:
+		english = 'five'
+	else:
+		english = 'forty-two'
+	print(english)
+	return english
+	
+awesome_student = {
+	"name": "Pint",
+	"city": "Luden",
+	"country": "Nooland"
+}
